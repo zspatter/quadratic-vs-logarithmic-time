9d788b1d8ca8a8c3c36c3afbde4d04a4cfef160c
Zachary Patterson <zach.s.patterson@gmail.com>
2019-04-13 16:50:00 -0400

    Removes sorts used for output comparison

--- time_complexity_comparison/time_complexity_comparison.py
+++ time_complexity_comparison/time_complexity_comparison.py
@@ -3,7 +3,7 @@ numberOfCompMerge, numberOfSwapsMerge, counter = 0, 0, 0
 
 def insertion_sort(collection):
     """
-    Counts the number of comparisons (conditions checked) and swaps
+    Counts the number of comparisons (between two elements) and swaps
     between elements while performing an insertion sort
 
     :param collection: a collection of comparable elements
@@ -32,88 +32,47 @@ def insertion_sort(collection):
 
 
 def merge_sort(collection):
-    comparisons, swaps = 0, 0
-    if len(collection) > 1:
-        mid = len(collection) // 2
-        left, right = collection[:mid], collection[mid:]
+    """
+    Counts the number of comparisons (between two elements) and swaps
+    between elements while performing a merge sort
 
-        left_part, right_part = merge_sort(left), merge_sort(right)
-        comparisons += left_part[0] + right_part[0]
-        swaps += left_part[1] + right_part[1]
-        sorted_collection = list()
-
-        while left and right:
-            if left[0] < right[0]:
-                sorted_collection.append(left.pop(0))
-            else:
-                sorted_collection.append(right.pop(0))
-            comparisons, swaps = comparisons + 1, swaps + 1
-
-        sorted_collection += (left or right)
-        return comparisons, swaps, sorted_collection
-
-        # while x < len(left) and y < len(right):
-        #     if left[x] < right[y]:
-        #         collection[z] = left.pop(0)
-        #         x += 1
-        #     else:
-        #         collection[z] = right.pop(0)
-        #         y += 1
-        #
-        #     comparisons += 1
-        #     swaps += 1
-        #     z += 1
-        # # comparisons += 1
-        #
-        # while x < len(left):
-        #     collection[z] = left[x]
-        #     x += 1
-        #     z += 1
-        #
-        #     # comparisons += 1
-        #     # swaps += 1
-        #
-        # # comparisons += 1
-        #
-        # while y < len(right):
-        #     collection[z] = right[y]
-        #     y += 1
-        #     z += 1
-
-            # comparisons += 1
-            # swaps += 1
-
-        # comparisons += 1
-
-    # else:
-        # comparisons += 1
-
-    # return comparisons, swaps
-
-
-def mergesort(collection):
+    :param collection: a collection of comparable elements
+    :return: total number of comparisons and swaps, and the sorted list
+    """
     comparisons, swaps = 0, 0
     mid = len(collection) // 2
     left, right = collection[:mid], collection[mid:]
 
     if len(left) > 1:
-        left_result = mergesort(left)
-        comparisons, swaps = comparisons + left_result[0], swaps + left_result[1]
+        left_result = merge_sort(left)
+        comparisons += left_result[0]
+        swaps += left_result[1]
         left = left_result[2]
     if len(right) > 1:
-        right_result = mergesort(right)
-        comparisons, swaps = comparisons + right_result[0], swaps + right_result[1]
+        right_result = merge_sort(right)
+        comparisons += right_result[0]
+        swaps += right_result[1]
         right = right_result[2]
 
-    result = []
+    sorted_collection = list()
+
+    # while elements in both sub lists
     while left and right:
         if left[0] <= right[0]:
-            result.append(left.pop(0))
+            sorted_collection.append(left.pop(0))
         else:
-            result.append(right.pop(0))
+            sorted_collection.append(right.pop(0))
         comparisons, swaps = comparisons + 1, swaps + 1
-    result += (left or right)
-    return comparisons, swaps, result
+
+    # if elements are left in only one sublist, merge them (guaranteed sorted)
+    if left:
+        sorted_collection += left
+        swaps += 1
+    if right:
+        sorted_collection += right
+        swaps += 1
+
+    return comparisons, swaps, sorted_collection
 
 
 def print_results(comparisons, swaps, sorted_collection):
@@ -128,14 +87,14 @@ for x in reversed(range(100)):
 comparison, swap = insertion_sort(collections)
 print_results(comparison, swap, collections)
 
+
 collections = []
-for x in reversed(range(100)):
+for x in (range(100)):
     collections.append(x)
 comparison, swap, result = merge_sort(collections)
 print_results(comparison, swap, result)
 
 collections = []
-for x in reversed(range(100)):
+for x in (range(100)):
     collections.append(x)
-comparison, swap, result = mergesort(collections)
-print_results(comparison, swap, result)
+
